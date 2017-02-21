13b12c3e0f094dd11f6927aa60524b6ba09cc5fd
Andrew Cabey <17acabey@gmail.com>
2017-02-21 12:42:27 -0500

    Implemented DriveToggleDirection
    Not functional! Rotation must be reversed

---
+++ src/com/team1757/commands/DriveToggleDirection.java
@@ -0,0 +1,38 @@
+package com.team1757.commands;
+
+import com.team1757.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveToggleDirection extends Command {
+
+	public DriveToggleDirection() {
+		requires(Robot.driveTrain);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.driveTrain.toggleMotorsDirection();
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
