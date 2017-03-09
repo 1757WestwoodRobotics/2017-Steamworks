7b2a1580c39ada78006930b74d6b38d17df01bce
Andrew Cabey <17acabey@gmail.com>
2017-03-08 19:32:28 -0500

    CGAutoRedBlue2 proper direction

--- src/com/team1757/commands/CGAutoRedBlue2.java
+++ src/com/team1757/commands/CGAutoRedBlue2.java
@@ -8,25 +8,27 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class CGAutoRedBlue2 extends CommandGroup {
 
-    public CGAutoRedBlue2() {
-//		Drop gear to receive
-    	addSequential(new GearReceive());    	
-//    	Drive straight ~100"
-    	addSequential(new DriveStraight(1.65));
-//    	Correct translation with vision
-    	addSequential(new VisionFaceGearTarget());
-//    	Drive straight ~14.3"
-    	addSequential(new DriveStraight(.1));
-//    	Place gear
-    	addSequential(new GearScore());
-//    	Close gear
-    	addSequential(new GearReceive());
-//		Reverse
-    	addSequential(new DriveToggleDirection());
-//    	Drive backwards ~100"
-    	addSequential(new DriveStraight(1.65));
-//    	Rotate left 90
-    	SmartDashboard.putNumber("targetNumber", 90);
-    	addSequential(new RotateToAngle());
-    }
+	public CGAutoRedBlue2() {
+		// Face gear loader forward
+		addSequential(new DriveDirectionInverted());
+		// Drop gear to receive
+		addSequential(new GearReceive());
+		// Drive straight ~100"
+		addSequential(new DriveStraight(1.65));
+		// Correct translation with vision
+		addSequential(new VisionFaceGearTarget());
+		// Drive straight ~14.3"
+		addSequential(new DriveStraight(.1));
+		// Place gear
+		addSequential(new GearScore());
+		// Close gear
+		addSequential(new GearReceive());
+		// Reverse
+		addSequential(new DriveToggleDirection());
+		// Drive backwards ~100"
+		addSequential(new DriveStraight(1.65));
+		// Rotate left 90
+		SmartDashboard.putNumber("targetNumber", 90);
+		addSequential(new RotateToAngle());
+	}
 }
