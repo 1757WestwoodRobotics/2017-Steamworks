d8d9900af90ccc76ece43c5f22443b06a547a656
Larry Tseng <lt222888@gmail.com>
2017-03-31 14:14:14 -0400

    Working ultrasonic sensor on arduino

--- arduino/UltrasonicPing/UltrasonicPing.ino
+++ arduino/UltrasonicPing/UltrasonicPing.ino
@@ -1,4 +1,4 @@
-// ---------------------------------------------------------------------------
+                                                                                                // ---------------------------------------------------------------------------
 // Example NewPing library sketch that does a ping about 20 times per second.
 // ---------------------------------------------------------------------------
 
@@ -6,12 +6,13 @@
 
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MIN_DISTANCE 2   // Minimum distance the sensor can detect (in centimeters).
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
-  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
 }
 
 void loop() {
