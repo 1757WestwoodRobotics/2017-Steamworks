59f309e2e9cf191e60c16ff9a694cfc4f6e3ac4e
Larry Tseng <lt222888@gmail.com>
2017-04-05 18:17:00 -0400

    Add enum register of addresses

--- arduino/i2c_arduino/i2c_arduino.ino
+++ arduino/i2c_arduino/i2c_arduino.ino
@@ -1,16 +1,5 @@
 // http://dsscircuits.com/articles/arduino-i2c-slave-guide
 
-// -------------------------
-// | Map Table             |
-// | Address   Descriptor  |
-// |-----------------------|
-// | 0    | Light1 Set     |
-// | 1    | Light2 Set     |
-// | 2    | Ultrasonic     |
-// | 3    | ---            |
-// | 4    | ---            |
-// -------------------------
-
 /**
  * - array of commands 
  * - commands[0] - arduino address
@@ -21,11 +10,27 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS   8
-#define REG_SIZE        
+#define REG_SIZE        10
+
+// enum of addresses and data
+
+enum addressRegister {
+  LED1_STATUS = 0x01,
+  LED2_STATUS = 0x02,
+  LED1_HUE_SET = 0x03,
+  LED1_SAT_SET = 0x04,
+  LED1_VAL_SET = 0x05, // 225 = full, 0 = off
+  LED2_HUE_SET = 0x06,
+  LED2_SAT_SET = 0x07,
+  LED2_VAL_SET = 0x08,
+  ULTRASONIC_CM = 0x09, 
+};
 
 byte sendCommands[3*8];
-byte receivedCommands[3*8];
+byte receivedCommands[3*8]; // [0] = address [1-24] = data
+
 int ultrasonicDistance;
+boolean commandRequested = false;
 
 void setup() {
   Wire.begin(SLAVE_ADDRESS);                // join i2c bus with address #8
@@ -41,11 +46,21 @@ void loop() {
 //    Serial.print(": ");
 //    Serial.println(receivedCommands[a]);
 //  }
-  Serial.println(ultrasonicDistance);
+//  Serial.println(ultrasonicDistance);
+  commandRequested = false;
 }
 
 void requestEvent() {
-  // Wire.write(registerMap, REG_MAP_SIZE);
+  
+  switch (receivedCommands[0]) {
+    case 0x00:
+      // null
+      break;
+    case 1:
+      break;
+  }
+  
+  // Wire.write(registerMap, REG_MAP_SIZE); 
 }
 
 void receiveEvent(int bytesReceived) {
@@ -55,32 +70,18 @@ void receiveEvent(int bytesReceived) {
     //Serial.println(receivedCommands[i]);
   }
 
-  ultrasonicDistance = unpack(recievedCommands);
-
-  switch (receivedCommands[0]) {
-    case 0:
-      Serial.println("case 0");
-      break;
-    case 1:
-      Serial.println("case 1");
-      break;
-    case 2:
-      Serial.println("case 2");
-      break;
-    default:
-      break;
-  }
-  
+  commandRequested = true;
+  // ultrasonicDistance = unpackData(receivedCommands); //mpack data?
 }
 
-void storeData(byte[] target, int data) {
+void storeData(byte target[], int data) {
   target[0] = (data & 0x000000FF) >> 0;
   target[1] = (data & 0x0000FF00) >> 8;
   target[2] = (data & 0x00FF0000) >> 16;
   target[3] = (data & 0xFF000000) >> 24;
 }
 
-int unpackData(byte[] data) {
+int unpackData(byte data[]) {
   return (data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24));
 }
 
