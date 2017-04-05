ca01f15420fb3233e555944b6e8eb4ac1cfde38b
Larry Tseng <lt222888@gmail.com>
2017-04-04 20:06:42 -0400

    Test I2C

--- arduino/i2c_arduino/i2c_arduino.ino
+++ arduino/i2c_arduino/i2c_arduino.ino
@@ -1,7 +1,7 @@
 // http://dsscircuits.com/articles/arduino-i2c-slave-guide
 
 // -------------------------
-// | Register Map          |
+// | Map Table             |
 // | Address   Descriptor  |
 // |-----------------------|
 // | 0    | Light1 Set     |
@@ -11,14 +11,20 @@
 // | 4    | ---            |
 // -------------------------
 
+/**
+ * - array of commands 
+ * - commands[0] - arduino address
+ * - commands[1] - component address
+ * - commands[2] - data
+ */
+ 
 #include <Wire.h>
 
 #define SLAVE_ADDRESS   8
 #define REG_MAP_SIZE    4
 #define MAX_SENT_BYTES  3
 
-byte registerMap[REG_MAP_SIZE];
-
+byte receivedCommands[3];
 int ultrasonicDistance;
 
 void setup() {
@@ -29,41 +35,37 @@ void setup() {
 }
 
 void loop() {
-  ultrasonicDistance = getUltrasonicDistance();
+  // ultrasonicDistance = getUltrasonicDistance();
 }
 
 void requestEvent() {
-  Wire.write(registerMap, REG_MAP_SIZE);
+  // Wire.write(registerMap, REG_MAP_SIZE);
 }
 
 void receiveEvent(int bytesReceived) {
   
   for (int i=0; i<bytesReceived; i++) {
-    Serial.println(Wire.read());
-    if (i < MAX_SENT_BYTES) {
-      receivedCommands[i] = Wire.read();
-    } else {
-      Wire.read();
-    }
+    receivedCommands[i] = Wire.read();
+    Serial.println(receivedCommands[i]);
   }
 
-  switch (receivedCommands[0]) {
-    case 0:
-      zeroZeroData = receivedCommands[1]; // set Address
-      return;
-      break;
-    case 1:
-      zeroOneData = receivedCommands[1];
-      return;
-      break;
-    default:
-      return;
-  }
+//  switch (receivedCommands[0]) {
+//    case 0:
+//      zeroZeroData = receivedCommands[1]; // set Address
+//      return;
+//      break;
+//    case 1:
+//      zeroOneData = receivedCommands[1];
+//      return;
+//      break;
+//    default:
+//      return;
+//  }
   
 }
 
 void storeData() {
-  registerMap[0] = ultrasonicDistance;
+  
 }
 
 /**
