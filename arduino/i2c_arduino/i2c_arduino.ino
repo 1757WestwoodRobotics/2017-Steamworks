b1f6aec7321861c0f6eac85a266df3bfa35f8c9f
Larry Tseng <lt222888@gmail.com>
2017-04-05 21:56:52 -0400

    Change to fixed width - cabey

--- arduino/i2c_arduino/i2c_arduino.ino
+++ arduino/i2c_arduino/i2c_arduino.ino
@@ -15,22 +15,24 @@
 // enum of addresses and data
 
 enum addressRegister {
-  LED1_STATUS = 0x01,
-  LED2_STATUS = 0x02,
-  LED1_HUE_SET = 0x03,
-  LED1_SAT_SET = 0x04,
-  LED1_VAL_SET = 0x05, // 225 = full, 0 = off
-  LED2_HUE_SET = 0x06,
-  LED2_SAT_SET = 0x07,
-  LED2_VAL_SET = 0x08,
-  ULTRASONIC_CM = 0x09, 
+  LED1_STATUS = 1,
+  LED2_STATUS = 2,
+  LED1_HUE_SET = 3,
+  LED1_SAT_SET = 4,
+  LED1_VAL_SET = 5, // 225 = full, 0 = off
+  LED2_HUE_SET = 6,
+  LED2_SAT_SET = 7,
+  LED2_VAL_SET = 8,
+  ULTRASONIC_CM = 9, 
 };
 
-byte sendCommands[3*8];
-byte receivedCommands[3*8]; // [0] = address [1-24] = data
+uint8_t sendCommands[2*4];
+uint8_t receivedCommands[2*4]; // [0] = address
 
-int ultrasonicDistance;
+uint32_t ultrasonicDistance;
 boolean commandRequested = false;
+
+uint8_t arr[4]; 
 
 void setup() {
   Wire.begin(SLAVE_ADDRESS);                // join i2c bus with address #8
@@ -39,24 +41,51 @@ void setup() {
   Serial.begin(9600);           // start serial for output
 }
 
+void storeData(byte target[], uint32_t data) {
+  Serial.println(151192065);
+  Serial.println(data);
+  Serial.println((data & 0x000000FF) >> 0);
+  Serial.println(data);
+  Serial.println((data & 0x0000FF00) >> 8);
+  Serial.println(data);
+  Serial.println((data & 0x00FF0000) >> 16);
+  Serial.println(data);
+  Serial.println((data & 0xFF000000) >> 24);
+  Serial.println(data);
+  target[0] = (data & 0x000000FF) >> 0;
+  target[1] = (data & 0x0000FF00) >> 8;
+  target[2] = (data & 0x00FF0000) >> 16;
+  target[3] = (data & 0xFF000000) >> 24;
+}
+
+uint32_t unpackData(uint8_t data[]) {
+  uint32_t i = (uint32_t) data[3];
+  i << 24;
+  i |= (uint32_t) data[2];
+  i << 16;
+  i |= (uint32_t) data[1];
+  i << 8;
+  i |= (uint32_t) data[0];
+  return i;
+}
+
 void loop() {
-  // ultrasonicDistance = getUltrasonicDistance();
-//  for (int a=0; a<3; a++) {
-//    Serial.print(a);
-//    Serial.print(": ");
-//    Serial.println(receivedCommands[a]);
-//  }
-//  Serial.println(ultrasonicDistance);
+//   for (int i = 0; i < 4; ++i) {
+//    Serial.println(arr[i]);
+//   }
+  // Serial.println(unpackData(arr));
   commandRequested = false;
 }
 
 void requestEvent() {
   
   switch (receivedCommands[0]) {
-    case 0x00:
+    case 0:
       // null
       break;
-    case 1:
+    case 9:
+      storeData(arr, 151192065); // 0x01020309
+      Wire.write(arr, 4);
       break;
   }
   
@@ -70,19 +99,11 @@ void receiveEvent(int bytesReceived) {
     //Serial.println(receivedCommands[i]);
   }
 
+//  Serial.println(unpackData(&receivedCommands[0])); // gets address
+//  Serial.println(unpackData(&receivedCommands[5]));
+
   commandRequested = true;
-  // ultrasonicDistance = unpackData(receivedCommands); //mpack data?
-}
-
-void storeData(byte target[], int data) {
-  target[0] = (data & 0x000000FF) >> 0;
-  target[1] = (data & 0x0000FF00) >> 8;
-  target[2] = (data & 0x00FF0000) >> 16;
-  target[3] = (data & 0xFF000000) >> 24;
-}
-
-int unpackData(byte data[]) {
-  return (data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24));
+  // ultrasonicDistance = unpackData(receivedCommands); //pack data?
 }
 
 /**
